6aff53b64114d553c6f47eecee82018466d01dfc
Dominic Chapman <dominicchapman@me.com>
2017-10-05 22:06:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+
+def prime?(test_case)
+  # prime must be greater than 1
+  if test_case <= 1
+    return false
+  end
+  # start check at 2 to ignore 1
+  i = 2
+  # keep count of any cases where division leaves no remainder
+  divisible_count = 0
+  while i < test_case
+    if test_case % i == 0
+      divisible_count += 1
+    end
+    # increment i to avoid infinite loop
+    i += 1
+  end
+  count == 0 ? true : false
+end
